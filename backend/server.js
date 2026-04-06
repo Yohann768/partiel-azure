25dd428fff641faf20c283b4606c164f85625744
Yohann <yohann.francisco88@gmail.com>
2026-04-06 23:33:47 +0200

    modif code

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,22 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// On autorise le front-end à communiquer avec le back-end
 app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello depuis le Back-end Node.js !" });
+  const dbConnectionString = process.env.AZURE_PARTIEL;
+
+  if (dbConnectionString) {
+    res.json({
+      message:
+        "Hello depuis Azure !  Le Back-end a bien récupéré le secret de la BDD depuis le Key Vault !",
+    });
+  } else {
+    res.json({
+      message:
+        "Hello depuis Azure !  Erreur : Le secret du Key Vault est introuvable.",
+    });
+  }
 });
 
 app.listen(port, () => {
